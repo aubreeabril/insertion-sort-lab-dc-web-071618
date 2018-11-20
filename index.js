2cfdf78a67b11cced5c56ae18708e6c8a38b5883
aubreeabril <aubree.abril@gmail.com>
2018-11-20 14:44:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let min = array[0];
+  let index = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      index = i;
+    }
+  }
+  array.splice(index, 1);
+  return min;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let sorted = [];
+  while (array.length > 0) {
+    let min = findMinAndRemove(array);
+    sorted.push(min);
+  }
+  return sorted;
 }
